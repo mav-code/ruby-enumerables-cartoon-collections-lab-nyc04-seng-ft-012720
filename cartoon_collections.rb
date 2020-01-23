d35928bd1703efef12559c2b4f5c092d2cddc064
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-23 01:41:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map{|element| array.element = element.capitalize
+  array.map{|element| array.element = element.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
