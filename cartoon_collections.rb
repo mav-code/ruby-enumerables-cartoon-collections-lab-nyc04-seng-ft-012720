0dbda2d0fd2922d3e1b20258b02b20411c685ded
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-23 01:45:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(words)
   words.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(# code an argument here)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
