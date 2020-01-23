b46bc98c594307d9309f2498204e2fc4f079619e
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-23 01:36:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(array)
   array.map{|dwarf| puts "#{array.index(dwarf) + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(# code an argument here)
+  array.map{
 end
 
 def long_planeteer_calls# code an argument here
