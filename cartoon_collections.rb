c86aa235be9281daefaa431adb3588ee1c5b8c6b
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-23 01:49:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect{|element| cheese_types.any?{|cheese|}}
+  array.detect{|element| cheese_types.include?(element)}
 end
